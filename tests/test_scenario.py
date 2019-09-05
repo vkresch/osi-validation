f0fea53843099157b35c6b93269d296f80d3a141
Viktor Kreschenski <viktor.kreschenski@altran.com>
2019-09-05 13:35:52 +0200

    Adjusted test

--- tests/test_scenario.py
+++ tests/test_scenario.py
@@ -8,7 +8,7 @@ class TestDataContainer(unittest.TestCase):
     """Test class of OSIScenario class"""
 
     def setUp(self):
-        self.MESSAGE_LENGTH = 50
+        self.MESSAGE_LENGTH = 15
         self.odc = OSIScenario()
         self.odc.from_file(path="data/small_test.txt",
                            type_name="SensorView")
